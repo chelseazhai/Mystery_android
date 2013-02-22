bc75e7a2f7db76300e4567306f7bed92d0f2975e
chelsea zhai <chelsea.zhai@gmail.com>
2013-02-22 13:16:40 +0800

    update mystery project

--- src/com/richitec/mystery/ContactsListActivity.java
+++ src/com/richitec/mystery/ContactsListActivity.java
@@ -1,20 +1,14 @@
 package com.richitec.mystery;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
 
 public class ContactsListActivity extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.contacts_list_activity_layout);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.contacts_list_activity_layout);
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.contacts_list_activity_layout, menu);
-        return true;
-    }
 }
